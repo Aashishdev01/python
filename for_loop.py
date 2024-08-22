2f410cdb29684d9949769c0473b74247252fb83d
Aashishdev01 <aashish456g@gmail.com>
2024-08-22 09:19:17 +0530

    Create for_loop.py

---
+++ for_loop.py
@@ -0,0 +1,12 @@
+for i in 'www.p4n.in':
+  print(i)
+
+# use range(start,end) function
+for i in range(10,21):
+  print(i)
+
+start = int(input('Enter Start Number : '))
+end = int(input('Enter End Number : '))
+
+for i in range(start,end+1):
+  print(i,end=' ')
