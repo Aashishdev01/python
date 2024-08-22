839fa96c7f7d4bcbeffbc4d4ae9315d4b89a13a3
Aashishdev01 <aashish456g@gmail.com>
2024-08-22 09:17:29 +0530

    Create if_elif.py
    The elif statement, short for "else if," is used to check multiple conditions. It is used after an if statement and before an else statement.
    
    Syntax
    if condition1:
        # block of code to be executed if condition1 is true
    elif condition2:
        # block of code to be executed if condition2 is true
    else:
        # block of code to be executed if none of the above conditions are true

---
+++ if_elif.py
@@ -0,0 +1,10 @@
+number1=5
+number2=10
+number3=25
+
+if number1 < number2 and number1 < number3:
+  print('Number 1 is smallest ....')
+elif number2 < number1 and number2 < number3:
+  print('Number 2 is smallest ... ')
+else:
+  print('Number 3 is smallest ....')
