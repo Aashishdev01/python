6a7551017b405c75021020101a6da024349d5289
Aashishdev01 <aashish456g@gmail.com>
2024-08-22 09:24:49 +0530

    function.py

---
+++ function.py
@@ -0,0 +1,30 @@
+ basic function
+
+def printMsg():
+  print('welcome to codeswithpankaj')
+
+# calling function
+
+printMsg()
+printMsg()
+# with return type
+
+def get_number():
+  return 900
+
+def web():
+  return "p4n.in"
+
+
+# calling function
+values = get_number()
+
+print(values)
+
+print(web())
+
+# with arguments
+
+def add(num1 , num2):
+  result = num1 + num2
+  print('this is result : ',result)
